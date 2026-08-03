303f415a64d9af881dc73bf0713cd371e0a06d7f
Dhruv N <tankig912@gmail.com>
2026-08-04 02:59:05 +0530

    add desc for ff and fg

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         -- Telescope settings | Fuzzy Finding
         local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Fuzzy find" })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Fuzzy grep" })
     end
 }
