a7b43daae205c345a32b5acaa39cb2aea438f4bc
Dhruv N <tankig912@gmail.com>
2026-09-05 12:24:16 +0530

    change theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,8 +1,8 @@
 return {
-    -- "catppuccin/nvim",
-    -- name = "catppuccin",
-    -- priority = 0,
-    -- config = function()
-    --     vim.cmd.colorscheme "catppuccin"
-    -- end
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 0,
+    config = function()
+        vim.cmd.colorscheme "catppuccin"
+    end
 }
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,21 +1,21 @@
 return {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-        transparent = true,
-        styles = {
-            sidebars = "transparent",
-            floats = "transparent",
-        },
-        on_highlights = function(hl, c)
-            hl.NeoTreeNormal = { bg = "none" }
-            hl.NeoTreeNormalNC = { bg = "none" }
-            hl.NeoTreeEndOfBuffer = { bg = "none" }
-        end,
-    },
-    config = function(_, opts)
-        require("tokyonight").setup(opts)
-        vim.cmd.colorscheme("tokyonight")
-    end,
+    -- "folke/tokyonight.nvim",
+    -- priority = 1000,
+    -- lazy = false,
+    -- opts = {
+    --     transparent = true,
+    --     styles = {
+    --         sidebars = "transparent",
+    --         floats = "transparent",
+    --     },
+    --     on_highlights = function(hl, c)
+    --         hl.NeoTreeNormal = { bg = "none" }
+    --         hl.NeoTreeNormalNC = { bg = "none" }
+    --         hl.NeoTreeEndOfBuffer = { bg = "none" }
+    --     end,
+    -- },
+    -- config = function(_, opts)
+    --     require("tokyonight").setup(opts)
+    --     vim.cmd.colorscheme("tokyonight")
+    -- end,
 }
