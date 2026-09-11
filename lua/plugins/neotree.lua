5f677c0b1d4e46b797924ce524c73c0819d3a23e
Dhruv N <tankig912@gmail.com>
2026-09-12 01:21:07 +0530

    show dot-files and .gitignored file in neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,20 +6,24 @@ return {
         "MunifTanjim/nui.nvim",
         "nvim-tree/nvim-web-devicons",
     },
+
+    lazy = false,
+
     opts = {
         filesystem = {
             filtered_items = {
-	            visible = true,
-	            show_hidden_count = false,
-	            hide_dotfiles = false,
-	            hide_gitignored = false,
-            	hide_by_name = {},
+                visible = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_by_name = {},
                 never_show = {},
             },
         },
     },
-    lazy = false,
-    config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
-    end
+
+    config = function(_, opts)
+        require("neo-tree").setup(opts)
+
+        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+    end,
 }
